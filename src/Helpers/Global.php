0cea61901b178536059ca7e88b0447d60a86203c
Kristers Zarins <kristerskz@gmail.com>
2017-11-09 14:28:47 +0200

    Define trans_model only if it is not defined in project itself

--- src/Helpers/Global.php
+++ src/Helpers/Global.php
@@ -1,27 +1,32 @@
 <?php
+
 use Netcore\Translator\Models\Language;
 
-/**
- * @param          $row
- * @param Language $language
- * @param String $attribute
- * @return String
- */
-function trans_model($row, Language $language, $attribute = null): String
-{
-    if (object_get($row, 'id')) {
-        if (is_null($attribute)) {
-            $model = $row->translateOrNew($language->iso_code);
+// In order to stay backwards compatible, we should define this
+// helper function only if it is not defined in project itself
+if (!function_exists('trans_model')) {
+    /**
+     * @param          $row
+     * @param Language $language
+     * @param String $attribute
+     * @return String
+     */
+    function trans_model($row, Language $language, $attribute = null): String
+    {
+        if (object_get($row, 'id')) {
+            if (is_null($attribute)) {
+                $model = $row->translateOrNew($language->iso_code);
 
-            if (is_object($model)) {
-                return $model;
+                if (is_object($model)) {
+                    return $model;
+                }
+            }
+
+            if ($row) {
+                return (string)$row->translateOrNew($language->iso_code)->$attribute;
             }
         }
 
-        if ($row) {
-            return (string)$row->translateOrNew($language->iso_code)->$attribute;
-        }
+        return '';
     }
-
-    return '';
 }
