0f376065c3ec2cc7482a4c738aaa7c579b0974e7
Viktors Ņikiforovs <viktors.nikiforovs@netcore.lv>
2018-05-16 20:08:18 +0300

    Sweetalert - promise instead of callback.

--- src/views/languages/scripts.blade.php
+++ src/views/languages/scripts.blade.php
@@ -1,81 +1,71 @@
 <script>
-    init.push(function () {
+	init.push(function () {
+		var dataTable = $('#languages-index-datatable');
 
-        if ($('#languages-index-datatable').length) {
+		if (dataTable.length) {
+			dataTable.dataTable({
+				responsive: true,
+				order: [[0, 'desc']], // Default is to order by Title
+				columns: [
+					{'orderable': true}, // Title
+					{'orderable': true}, // Title localized
+					{'orderable': true}, // ISO code
+					{'orderable': true}, // Fallback
+					{'orderable': true}, // Visible
+					{'orderable': false} // Actions
+				]
+			});
 
-            var dataTable = $('#languages-index-datatable').dataTable({
-                responsive: true,
+			$('#languages-index-datatable_wrapper .table-caption').text('{{ array_get($uiTranslations, 'languages') }}');
+			$('#languages-index-datatable_wrapper .dataTables_filter input').attr('placeholder', '{{ array_get($uiTranslations, 'search') }}');
+		}
 
-                "columns": [
-                    { "orderable": true }, // Title
-                    { "orderable": true }, // Title localized
-                    { "orderable": true }, // ISO code
-                    { "orderable": true }, // Fallback
-                    { "orderable": true }, // Visible
-                    { "orderable": false } // Actions
-                ],
+		$('body').on('click', '.translations-in-database-confirm-action', function (e) {
+			e.preventDefault();
 
-                order: [[ 0, 'desc' ]] // Default is to order by Title
-            });
+			var btn = $(this);
 
-            $('#languages-index-datatable_wrapper .table-caption').text('{{ array_get($uiTranslations, 'languages') }}');
-            $('#languages-index-datatable_wrapper .dataTables_filter input').attr('placeholder', '{{ array_get($uiTranslations, 'search') }}');
-        }
+			swal({
+				title: $(btn).data('title'),
+				text: $(btn).data('text'),
+				type: 'warning',
+				showCancelButton: true,
+				confirmButtonColor: '#DD6B55',
+				confirmButtonText: $(btn).data('confirm-button-text')
+			}).then(function () {
+				$.ajax({
+					url: btn.attr('href'),
+					type: btn.data('method'),
+					dataType: 'json',
 
+					success: function () {
+						window.setTimeout(function () {
+							swal({
+								title: btn.data('success-title'),
+								text: btn.data('success-text'),
+								type: 'success'
+							});
+						}, 100);
 
+						if (btn.data('id')) {
+							var element = $('.object' + btn.data('id'));
 
-        $('body').on('click', '.translations-in-database-confirm-action',function(e){
+							if (element.length) {
+								element.fadeOut();
+							} else {
+								btn.closest('tr').fadeOut();
+							}
+						}
+					},
 
-            console.log('specific package code');
-
-            e.preventDefault();
-
-            var btn = $(this);
-
-            swal({
-                title: $(btn).data('title'),
-                text: $(btn).data('text'),
-                type: "warning",
-                showCancelButton: true,
-                confirmButtonColor: "#DD6B55",
-                confirmButtonText: $(btn).data('confirm-button-text'),
-                closeOnConfirm: true
-            }, function(){
-
-                $.ajax({
-                    url: btn.attr('href'),
-                    type: btn.data('method'),
-                    dataType: 'json',
-
-                    success: function (response) {
-
-                        window.setTimeout(function () {
-                            swal({
-                                title: btn.data('success-title'),
-                                text: btn.data('success-text'),
-                                type: "success"
-                            });
-                        }, 100);
-
-                        if (btn.data('id')) {
-                            if ($('.object' + btn.data('id')).length) {
-                                $('.object' + btn.data('id')).fadeOut();
-                            }
-                            else {
-                                btn.closest('tr').fadeOut();
-                            }
-                        }
-
-                    },
-                    error: function (xhr) {
-                        console.log(xhr);
-                        window.setTimeout(function () {
-                            swal("Error", xhr.responseText, "error");
-                        }, 100);
-                    }
-                });
-
-            });
-        });
-    });
+					error: function (xhr) {
+						console.log(xhr);
+						window.setTimeout(function () {
+							swal('Error', xhr.responseText, 'error');
+						}, 100);
+					}
+				});
+			}).catch(function () {});
+		});
+	});
 </script>
